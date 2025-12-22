293e02d91e6a902e8c9b496907ab917a23156695
Hasan Demir <150049291+hasandemir34@users.noreply.github.com>
2025-12-22 11:07:10 +0300

    tamam 22.12

--- Models/Category.cs
+++ Models/Category.cs
@@ -5,7 +5,7 @@ namespace stokprojesi1.Models
     public class Category
 {
     [Key] //PK olarak düşünülebilir ve 1er 1er artar bu sayı.
-    public int CategoryId { get; set; }
+    public int CategoryId { get; set; } //ayarlama
 
     [Required(ErrorMessage = "Kategori adı boş bırakılamaz!")] //bu alanın zorunlu olduğunu anlıyorum buradan
     [Display(Name = "Kategori Adı")] //ekranda görünecek hali
